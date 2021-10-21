dda7a5842058a1830b62de60112d0d4a1272de8b
Joseph Quang <87520683+tquangsdh20@users.noreply.github.com>
2021-10-21 19:05:19 +0700

    Update example v1.1.0rc0

--- example/example.py
+++ example/example.py
@@ -1,12 +1,10 @@
-from memrise import Course,Database
+from memrise import Course, Data
+#Create file database output
+db = Data('English.sqlite') #Other format is .db
+#Connect to file database and init
+db.init_database()
 
-db = Database('English.sqlite')
-db.connect()
-db.init()
+#Connect the course to scraping info this maybe take a few momment.
 course = Course(1658724,2)
-course.update(db)
-levels = course.get_levels()
-for level in levels:
-    level.update(db)
-
-db.update_db_en(1658724,'fr')
+#Update information about the course
+db.update_course(course)
