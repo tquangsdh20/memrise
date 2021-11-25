8cc51f1a5035e225f99c4f3e6b86931ee601522b
Joseph Quang <tquangsdh20@hcmut.edu.vn>
2021-11-25 20:32:21 +0000

    release check

--- memrise/memrise.py
+++ memrise/memrise.py
@@ -10,26 +10,28 @@ class Data(_Data_):
 
     def update_level(self, level: Level) -> None:
         __level = level.get_record()
-        try:
-            self.__update(INSERT_LEVEL, __level)
-        except:
-            print("RecordError: Fail to update record", __level)
-        else:
-            self.conn.commit()
-        finally:
-            __words = level.get_words()
-            self._update(INSERT_WORD, level.get_words())
+        # try:
+        #     self.__update(INSERT_LEVEL, __level)
+        # except:
+        #     print("RecordError: Fail to update record", __level)
+        # else:
+        #     self.conn.commit()
+        # finally:
+        #     __words = level.get_words()
+        #     self._update(INSERT_WORD, level.get_words())
+
+        self._update(INSERT_LEVEL, __level)
+        self.conn.commit()
+        __words = level.get_words()
+        self._update(INSERT_WORD, level.get_words())
 
     def update_course(self, course: Course) -> None:
-        try:
-            __course = course.get_record()
-            self.__update(INSERT_COURSE, __course)
-        except:
-            print("RecordError: Fail to update record", course.get_record())
-        else:
+        __course = course.get_record()
+        self._update(INSERT_COURSE, __course)
+        # except:
+        # print("RecordError: Fail to update record", course.get_record())
+        self.conn.commit()
+        levels = course.get_levels()
+        for level in levels:
+            self.update_level(level)
             self.conn.commit()
-        finally:
-            levels = course.get_levels()
-            for level in levels:
-                self.update_level(level)
-                self.conn.commit()
